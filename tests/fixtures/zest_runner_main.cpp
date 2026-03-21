8fa5768c99b7a7ba9117a616935370b0bff8c63c
ykiko <ykikoykikoykiko@gmail.com>
2026-03-21 15:13:07 +0800

    test: remove zest fixture target

--- tests/fixtures/zest_runner_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "eventide/zest/zest.h"
-
-int main(int argc, char** argv) {
-    static_cast<void>(
-        eventide::zest::run_cli(argc,
-                                argv,
-                                "zest_runner_fixture [options] Run zest runner fixture"));
-    return 0;
-}
